f3bc99d5b06b51b9eb0e41bed78cc1a3eff7c247
jhun91 <fonercis@naver.com>
2020-10-18 20:01:17 +0900

    Initial commit

---
+++ src/test/kotlin/com/tistory/jeongdev/socchat/SocChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.jeongdev.socchat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
